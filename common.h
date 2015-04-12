9b77f0ae893378cd95930521dc89489e742b7927
Jbarron562 <toaster_crc@yahoo.com>
2015-04-12 15:24:52 -0700

    Update common.h

--- common.h
+++ common.h
@@ -1,3 +1,21 @@
+//***************************************************************************
+//  CODE FILENAME:  BarronValero-assn4-common.cpp
+//  DESCRIPTION:    Common Functions used in sorting of arrays, only the prototypes will be found here 
+//  TERM:	          Spring 8 weeks 2 2015
+/*			
+*/
+//  DESIGNER:       Eric Valero / Jose Barron
+//  FUNCTIONS:    
+/*		 
+		    displayHeader-		Displays a quick description of what the program does
+		    printDisplay-		  Displays a menu to the user to make a selection
+		    verifiySorted		  verifies array is sorted in ascending order
+		    choices			      It takes the string inputted by the user, and splits it into 2 characters
+		    parseChoice			  takes the choice made by the user and assigns it a string value
+		    executeSort			  calls other functions based on the users selection
+*/
+//***************************************************************************
+
 #include "valero-assn4.h"
 #include "barron-assn4.h"
 using namespace std;
@@ -23,7 +41,7 @@ void printDisplay();
 void displayHeader();
 
 //Function that takes a string and returns the two sort method choices
-char* choices(string s);
+char* choices(string);
 
 //Function that prompts user for number of times to execute sorts
 int sortInput();
@@ -32,4 +50,4 @@ int sortInput();
 int executeSort(char choice, int* arr, int size);
 
 //Function to return a string based on the sort char passed in
-string parseChoice(char c);
+string parseChoice(char);
