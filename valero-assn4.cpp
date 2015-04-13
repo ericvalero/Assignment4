4fd455e595350541df79f42944e64331caff9c53
Jbarron562 <toaster_crc@yahoo.com>
2015-04-12 17:43:35 -0700

    Update valero-assn4.cpp

--- valero-assn4.cpp
+++ valero-assn4.cpp
@@ -125,7 +125,7 @@ int* mergeSort(int* input, int pivot, int right)
 //**********************************************************************
 double averageResults(int* arr, int size)
 {
-	int average;
+	double average;
 	double total = 0.0;							//total initialized
 	for (int i = 0; i < size; i++)						
 	{
